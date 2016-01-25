e7fd59d973a011864c5de516d5fd1d2be22a2650
Krisztián Gödrei <godrekrisztian@gmil.com>
2016-01-25 17:30:51 +0100

    test fixes

--- CreditCardValidator.Droid.UITests/Tests.cs
+++ CreditCardValidator.Droid.UITests/Tests.cs
@@ -29,25 +29,22 @@ namespace CreditCardValidator.Droid.UITests
 		[SetUp]
 		public void BeforeEachTest()
 		{
-			// steps-xamarin-android-test exports the generated APK path.
 			string apkPath = Environment.GetEnvironmentVariable ("ANDROID_APK_PATH");
-
-			if (apkPath == null || apkPath == "") {
-				// steps-xamarin-test-cloud-for-android uploads the APK and assemblies to Xamarin Test Cloud.
-				// This case we grab the apk path from there. 
-				// see: https://forums.xamarin.com/discussion/26296/passing-the-apk-path-using-xamarin-uitest-on-test-cloud-with-android.
-				string currentFile = new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath;
-				string dir = new FileInfo(currentFile).Directory.Parent.Parent.Parent.FullName;
-				apkPath = Path.Combine(dir, "com.xamarin.example.creditcardvalidator-Signed.apk");
-			}
-
 			string emulatorSerial = Environment.GetEnvironmentVariable ("ANDROID_EMULATOR_SERIAL");
 
-			ConfigureApp.Android
-				.ApkFile(apkPath)
-				.DeviceSerial(emulatorSerial)
-				.WaitTimes(new WaitTimes())
-				.StartApp();
+			if (apkPath != null && apkPath != "" && emulatorSerial != null && emulatorSerial != "") {
+				ConfigureApp
+					.Android
+					.ApkFile (apkPath)
+					.DeviceSerial (emulatorSerial)
+					.WaitTimes(new WaitTimes())
+					.StartApp ();
+			} else {
+				ConfigureApp
+					.Android
+					.WaitTimes(new WaitTimes())
+					.StartApp ();
+			}
 		}
 
 		[Test]
